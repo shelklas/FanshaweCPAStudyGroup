f92b7efdafc1f9946058cda760c326f2da2a80a4
vaio <into.the.deepest.space@gmail.com>
2017-11-18 12:15:12 -0500

    added demo code, updated reference

--- 5055_review/tuple/tuple.cpp
+++ 5055_review/tuple/tuple.cpp
@@ -130,21 +130,6 @@
 *		get					Extracts the Ith element from the tuple. I is an 
 *							integer value in [0, sizeof...(Types)).
 *
-*			template< std::size_t I, class... Types >
-*			typename std::tuple_element<I, tuple<Types...> >::type&
-*				get(tuple<Types...>& t) noexcept;
-*			-	Returns element by reference
-*
-*			template< std::size_t I, class... Types >
-*			typename std::tuple_element<I, tuple<Types...> >::type&&
-*				get(tuple<Types...>&& t) noexcept;
-*			-	Returns an lvalue
-*
-*			template< std::size_t I, class... Types >
-*			typename std::tuple_element<I, tuple<Types...> >::type const&
-*				get(const tuple<Types...>& t) noexcept;
-*			-	Returns element by const reference
-*
 *		- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 *		operator==, != , <, <= , >, >=
 *
@@ -183,8 +168,143 @@
 
 #include <boost\test\unit_test.hpp>
 #include <tuple>
+#include <vector>
+
+struct foo {
+	int attr_;
+
+	foo(int a = 0) : attr_(a) {};
+};
+
+struct bar
+{
+	int attr1_, attr2_;
+
+	bar(std::pair<int, int> p = { 0, 0 }) : attr1_(p.first), attr2_(p.second) {};
+	bar(int a1, int a2) : attr1_(a1), attr2_(a2) {};
+};
 
 /* CONSTRUCTORS EXAMPLES */
 BOOST_AUTO_TEST_CASE(constuctors) {
 
+	{
+		std::tuple<int, long, double> tuple_default;
+	}
+
+	{
+		std::tuple<foo, int, std::vector<double>, bar> tuple_direct_init{ 1, 0, {1.0, -0.5, 3}, {3, 4} };
+		std::tuple<size_t, size_t, size_t> tuple_direct_init_2 = { 1, 1, 1 };
+	}
+
+	{
+		int i = 123456;
+		double d = 42.99999999;
+		foo f(987654);
+		std::tuple<int, double, foo> tuple(std::move(i), std::move(d), std::move(f));
+	}
+
+	{
+		std::tuple<long, short, double> tuple_direct_init(-1234, 543, 4.5);
+		std::tuple<long long, float, double> tuple_converting_copy(tuple_direct_init);
+
+		std::tuple<foo, int, double> tuple_converting_move(std::move(tuple_direct_init));
+	}
+
+	{
+		std::pair<foo, bar> pair(123456, { 7654, -7654 });
+		std::tuple<foo, bar> tuple_from_pair_copy(pair);
+		std::tuple<foo, bar> tuple_from_pair_move(std::move(pair));
+	}
+
+	{
+		std::tuple<int, double, foo> tuple_direct_init(1234, 543.9876543, foo(4545545));
+		std::tuple<int, double, foo> tuple_copy(tuple_direct_init);
+		std::tuple<int, double, foo> tuple_move(std::move(tuple_direct_init));
+	}
+}
+
+/* MEMBER FUNCTIONS EXAMPLES */
+BOOST_AUTO_TEST_CASE(member_functions) {
+
+	{
+		std::tuple<int, double, foo> tuple_copy_assignment;
+		std::tuple<int, double, foo> tuple_move_assignment;
+		std::tuple<int, double, foo> tuple(12345, 987654.987654, 876543);
+		tuple_copy_assignment = tuple;
+		tuple_move_assignment = std::move(tuple);
+	}
+
+	{
+		std::tuple<int, double, foo> tuple_converting_copy_assignment;
+		std::tuple<int, double, foo> tuple_converting_move_assignment;
+		std::tuple<short, float, foo> tuple(12345, 987654.987654f, 876543);
+		tuple_converting_copy_assignment = tuple;
+		tuple_converting_move_assignment = std::move(tuple);
+	}
+
+	{
+		std::tuple<int, foo> tuple_pair_copy_assignment;
+		std::tuple<int, int> tuple_pair_move_assignment;
+		std::pair<int, int> pair(12345, 876543);
+		tuple_pair_copy_assignment = pair;
+		tuple_pair_move_assignment = std::move(pair);
+	}
+
+	{
+		std::tuple<int, double, foo> tuple(123, 123, 123);
+		std::tuple<int, double, foo> other(321, 321, 321);
+		tuple.swap(other);
+	}
+}
+
+/* NON-MEMBER FUNCTIONS */
+BOOST_AUTO_TEST_CASE(non_member_functions) {
+
+	{
+		auto tuple = std::make_tuple(12345ull, foo(743), 3.f, std::vector<bar>{ {0, 0}, {1, 5} });
+	}
+
+	{
+		int i = 124;
+		foo f(421);
+		auto tuple = std::tie(i, f);
+	}
+
+	{
+		std::tuple<char, short, int> low;
+		std::tuple<long, long> mid;
+		std::tuple<float, double, bool> high;
+
+		auto tuple = std::tuple_cat(low, mid, high);
+	}
+
+	{
+		std::tuple<int, char, double> tuple;
+		auto e = std::get<2>(tuple);
+	}
+
+	{
+		std::tuple<long, int> tuple1(12, 5432);
+		std::tuple<long, int> tuple2(12, 2345);
+
+		bool e = tuple1 == tuple2;
+		bool l = tuple1 < tuple2;
+		bool g = tuple1 > tuple2;
+		bool le = tuple1 <= tuple2;
+		bool ge = tuple1 >= tuple2;
+		bool ne = tuple1 != tuple2;
+	}
+}
+
+BOOST_AUTO_TEST_CASE(helper_classes) {
+
+	{
+		std::tuple_size<std::tuple<int, long>> ts;
+		BOOST_CHECK_EQUAL(2, ts.value);
+	}
+
+	{
+		bool f = std::is_same<std::tuple_element<1, std::tuple<float, foo>>::type, foo>::value;
+		BOOST_CHECK(f);
+	}
 }
